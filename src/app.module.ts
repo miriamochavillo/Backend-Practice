44744c16f36133bea15ba6a258d345bd3adca73a
miriamochavillo <miriam.iochavillo@gmail.com>
2025-02-05 10:49:03 +0800

    Initital Commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
